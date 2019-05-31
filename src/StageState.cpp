d36bc0d5b51b28f2458386d6998ffca05389612d
luisfbgs <arueluis@gmail.com>
2019-05-31 07:03:46 -0300

    Sprites de contador para vida e novo tabuleiro

--- src/StageState.cpp
+++ src/StageState.cpp
@@ -26,7 +26,7 @@ StageState::~StageState() {
 void StageState::LoadAssets() {
     // Cria background e ajusta pro tamanho da janela
     GameObject *bgGO = new GameObject();
-    std::shared_ptr<Sprite> bgSprite(new Sprite(*bgGO, std::string("assets/img/board.jpeg")));
+    std::shared_ptr<Sprite> bgSprite(new Sprite(*bgGO, std::string("assets/img/board.png")));
     float bgScale = std::min((float)Game::GetInstance().GetWidth() / bgSprite->GetWidth(),
      (float)Game::GetInstance().GetHeight() / bgSprite->GetHeight());
     bgSprite->SetScale(bgScale, bgScale);
