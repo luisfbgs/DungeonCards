71b4ebf351e84faffa6c5c10a529f6fde8c5f189
luisfbgs <arueluis@gmail.com>
2019-07-12 11:22:59 -0300

    Novos sprites de jogadores, terceiro jogador adicionado, sprites de habilidade diferentes

--- include/Control.h
+++ include/Control.h
@@ -2,8 +2,8 @@
 
 #include <InputManager.h>
 
-int buttonUp[4] = {'w', UP_ARROW_KEY};
-int buttonDown[4] = {'s', DOWN_ARROW_KEY};
-int buttonLeft[4] = {'a', LEFT_ARROW_KEY};
-int buttonRight[4] = {'d', RIGHT_ARROW_KEY};
-int buttonAttack[4] = {'k', ' '};
+int buttonUp[4] = {'w', UP_ARROW_KEY, 'i'};
+int buttonDown[4] = {'s', DOWN_ARROW_KEY, 'k'};
+int buttonLeft[4] = {'a', LEFT_ARROW_KEY, 'j'};
+int buttonRight[4] = {'d', RIGHT_ARROW_KEY, 'l'};
+int buttonAttack[4] = {'q', ' ', 'u'};
