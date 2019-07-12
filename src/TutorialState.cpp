3a306fe387f345b65dad42a6edc96926ee36bb4b
maffei2443 <leoitu22hotmail.com@gmail.com>
2019-07-12 01:17:51 -0300

    add Setas às telas de tutorial

--- src/TutorialState.cpp
+++ src/TutorialState.cpp
@@ -15,11 +15,54 @@ void TutorialState::LoadAssets() {
     // Cria background e ajusta pro tamanho da janela
     std::shared_ptr<GameObject> bgGO = std::make_shared<GameObject>();
     std::shared_ptr<Sprite> bgSprite = std::make_shared<Sprite>(
-        bgGO, TUTORIAL_PATH + prefix + std::to_string(this->curr) + ".png");
+        bgGO, TUTORIAL_PATH + prefix + std::to_string(this->curr) + ".png"
+    );
+    std::shared_ptr<GameObject> leftArrowGO = std::make_shared<GameObject>();
+    std::shared_ptr<Sprite> leftArrowSprite = std::make_shared<Sprite>(
+        leftArrowGO, TUTORIAL_PATH "arrow.png"
+    );
+    std::shared_ptr<GameObject> rightArrowGO = std::make_shared<GameObject>();
+    std::shared_ptr<Sprite> rightArrowSprite = std::make_shared<Sprite>(
+        rightArrowGO, TUTORIAL_PATH "arrow.png"
+    );
+    // Sprite eh o mesmo, muda soh a rotacao
+    leftArrowSprite->SetAngle(90);
+    rightArrowSprite->SetAngle(-90);
+    float leftPadding = 0.1;
+    leftArrowGO->box.leftUp = {
+        ((float)(Game::widthS)) * 0.1,
+        ((float)(Game::heightS)) * 0.83
+    };
+    rightArrowGO->box.leftUp = {
+        ((float)(Game::widthS)) * 0.87,
+        ((float)(Game::heightS)) * 0.83
+    };
+
+    // std::shared_ptr<Sprite> rightArrow = std::make_shared<Sprite>(
+    //     bgGO, TUTORIAL_PATH "arrow.png"
+    // );
+
     float bgScale = Game::widthS / bgSprite->GetWidth();
+
     bgSprite->SetScale(bgScale, bgScale);
+    // Quer-se escala tal que caibam n setas empilhadas.]
+    int n = 8;
+    float arrowScale = (float)(Game::GetInstance().heightS)/(n * leftArrowSprite->GetHeight());
+
+    leftArrowSprite->SetScale(arrowScale,arrowScale);
+    rightArrowSprite->SetScale(arrowScale,arrowScale);
+
+    // leftArrowSprite
+
+
     bgGO->AddComponent(bgSprite);
+    bgGO->AddComponent(leftArrowSprite);
+    bgGO->AddComponent(rightArrowSprite);
+    
     this->AddObject(bgGO);
+    this->AddObject(leftArrowGO);
+    this->AddObject(rightArrowGO);
+
 }
 
 void TutorialState::Start() {
